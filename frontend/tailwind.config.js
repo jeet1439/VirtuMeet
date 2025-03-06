7e3a9fa8e9cc53c57431257dbe422821081882b4
jeet <jeetbanik332004@gmail.com>
2025-03-06 11:30:24 +0530

    tailwind-2

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,19 @@
-const flowbite = require("flowbite-react/tailwind");
+import flowbitePlugin from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-      flowbite.content()
-    ],
-    theme: {
-      extend: {
-        colors: {
-          customDark: '#1c1f2e', 
-        },
-        
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}", 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customDark: "#1c1f2e",
       },
     },
-    plugins: [
-      flowbite.plugin()
-    ],
-  }
+  },
+  plugins: [flowbitePlugin()], 
+};
